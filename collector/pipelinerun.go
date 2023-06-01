e1458e631093baba432d052907749e742ee0b8a4
gabemontero <gmontero@redhat.com>
2023-06-01 10:27:27 -0400

    switch to controller-runtime prometheus integration

--- collector/pipelinerun.go
+++ collector/pipelinerun.go
@@ -2,11 +2,14 @@ package collector
 
 import (
 	"context"
+	"github.com/go-kit/log/level"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"os"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 func SetupPipelineRunCachingClient(mgr ctrl.Manager) {
@@ -16,4 +19,10 @@ func SetupPipelineRunCachingClient(mgr ctrl.Manager) {
 	prList := v1beta1.PipelineRunList{}
 	opts := &client.ListOptions{Namespace: metav1.NamespaceAll}
 	mgr.GetClient().List(context.Background(), &prList, opts)
+	psCollector, err := NewCollector(logger, mgr.GetClient())
+	if err != nil {
+		level.Error(logger).Log("msg", "Couldn't create collector", "error", err)
+		os.Exit(1)
+	}
+	metrics.Registry.MustRegister(psCollector)
 }
